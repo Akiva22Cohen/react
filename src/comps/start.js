a002fcee56dd16624272eeda3781ebdac4e1033e
Akiva Cohen <akiva.coen@gmail.com>
2023-08-06 07:46:39 +0300

    On branch start_default Changes to be committed:     modified:   public/index.html 	modified:   src/App.js 	new file:   src/comps/start.js 	modified:   src/index.js

---
+++ src/comps/start.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Start() {
+  return (
+    <div className="Start">
+      <h2 className='text-bg-danger'>start!</h2>
+    </div>
+  );
+}
+
+export default Start;
