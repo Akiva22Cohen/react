2a3cd91e353b4acaacab64bc04615d5efe2285c3
Akiva22Cohen <141541292+Akiva22Cohen@users.noreply.github.com>
2023-10-15 19:05:31 +0300

    . Lines starting On branch Weather Your branch is ahead of 'origin/Weather' by 1 commit.    (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/components/datalist.js modified:   src/countries.json

--- src/components/datalist.js
+++ src/components/datalist.js
@@ -1,23 +1,12 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import arr from '../countries.json';
 
 function Datalist() {
-    const [ar, setAr] = useState();
-    useEffect(() => {
-        doApi();
-    }, []);
-
-    const doApi = async () => {
-        const url = 'https://restcountries.com/v3.1/all?fields=name';
-        const resp = await axios.get(url);
-        const data = await resp.data;
-        // console.log(data[0]['name']['common']);
-        setAr(() => data.map(({ name }) => name['common']));
-    };
-
     return (
-        ar &&
-        ar.map(item => <option key={item} className='me-1'>{item}</option>)
+        <>
+            {arr.cities.sort().map((item, i) => <option key={i}>{item}</option>)}
+            {arr.countries.sort().map(item => <option key={item}>{item}</option>)}
+        </>
     );
 }
 
